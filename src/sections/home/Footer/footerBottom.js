d98e3493036c0d7625bac6951462d52c1b0eb0de
mamun4023 <mamun754023@gmail.com>
2023-01-24 19:08:33 +0600

    fixed footer

--- src/sections/home/Footer/footerBottom.js
+++ src/sections/home/Footer/footerBottom.js
@@ -160,7 +160,7 @@ export default function FooterBottom(){
             </div> */}
 
             {/*  */}
-            <div className="grid grid-cols-1 md:grid-cols-6">
+            <div className="grid grid-cols-1 xl:grid-cols-6">
                 <div className="md:col-span-2"> 
                    
                     {/* logo and social logo */}
@@ -171,7 +171,7 @@ export default function FooterBottom(){
                                 className = "w-40"
                             />
                         </div>
-                        <div className="my-6 w-full pr-2 ">
+                        <div className="my-6 w-full pr-2 md:w-7/12">
                             <p className="text-sm text-fontColor font-sans_regular leading-5">An online platform that encourages travel by removing stress from personal, leisure travel planning. </p>
                         </div>  
                         <div className="flex justify-center md:justify-start lg:justify-start lg:flex-none mt-2">
@@ -201,16 +201,16 @@ export default function FooterBottom(){
                     <div>
                         <h2 className="font-readex_bold text-fontColor" > About </h2>
                         <div>
-                            <div className="mt-2">  
+                            <div className="mt-2 leading-4">  
                                 <a className="text-xs text-fontColor hover:underline" href="#"> About Intripid </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Our Travelers</a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Contact Us</a>
                             </div>
-                            <div className="mt-2">     
+                            <div className="mt-2 leading-4">     
                                 <a className="text-xs text-fontColor hover:underline" href="#"> FAQs</a>
                             </div>
                         </div>
@@ -219,19 +219,19 @@ export default function FooterBottom(){
                     <div className="w-32 md:w-40">
                         <h2 className="font-readex_bold"> Solutions </h2>
                         <div>
-                            <div className="mt-2"> 
-                                <a className="text-xs text-fontColor hover:underline" href="#"> Personalized Destinations </a>
+                            <div className="mt-2 leading-4"> 
+                                <a className="text-xs text-fontColor leading-none hover:underline" href="#"> Personalized Destinations </a>
                             </div>
-                            <div className="mt-2">
+                            <div className="mt-2 leading-4">
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Expert Travel Advisors </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Corporate Travel Planning </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Travel Sponsorship </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> TravelPoints & Rewards <br/> Integration </a>
                             </div>
                         </div>
@@ -240,13 +240,13 @@ export default function FooterBottom(){
                     <div className="mt-4 lg:mt-0">
                         <h2 className=" font-readex_bold text-fontColor"> Pricing </h2>
                         <div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Membership Plans </a>
                             </div>
-                             <div className="mt-2"> 
+                             <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Deals & Packages </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Vouchers </a>
                             </div>                
                         </div>
@@ -255,10 +255,10 @@ export default function FooterBottom(){
                     <div className="w-32 md:w-40 mt-4 2xl:mt-0 ">
                         <h2 className=" font-readex_bold text-fontColor"> Resources </h2>
                         <div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Intripid Brochure </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Executive Cutsheet </a>
                             </div>
                         </div>
@@ -267,7 +267,7 @@ export default function FooterBottom(){
                     {/* col 5 */}
                     <div className="mt-4 2xl:mt-0">
                         <h2 className=" font-readex_bold text-fontColor"> Support </h2>
-                        <div className="mt-2">
+                        <div className="mt-2 leading-4">
                             <a className="text-xs text-fontColor hover:underline" href="#"> Help Center </a>
                         </div>
                     </div>
@@ -275,14 +275,14 @@ export default function FooterBottom(){
                     <div className="w-32 md:w-40 mt-4 2xl:mt-0 ">
                         <h2 className=" font-readex_bold text-fontColor">Privacy & Safety</h2>
                         <div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Terms & Conditions </a>
                             </div>
-                            <div className="mt-2"> 
+                            <div className="mt-2 leading-4"> 
                                 <a className="text-xs text-fontColor hover:underline" href="#"> Privacy & Cookies Statement </a>
                             </div>
-                            <div className="mt-2"> 
-                                <a className="text-sm text-fontColor hover:underline" href="#"> Security </a>
+                            <div className="mt-2 leading-4"> 
+                                <a className="text-xs text-fontColor hover:underline" href="#"> Security </a>
                             </div>
                         </div>
                     </div>
